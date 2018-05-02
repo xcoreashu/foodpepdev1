fb904790c02a2eaa50e81cac4aefa7353eefe405
xcoreashu <connorashutosh@gmail.com>
2018-05-02 19:01:39 +0530

    2nd commit

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-  res.send( {hi: 'there'} );
+  res.send( {hi: 'there'});
 });
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(5000);
