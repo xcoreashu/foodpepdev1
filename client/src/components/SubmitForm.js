a64a56bd66a7d2ae53745e61281a0e796821785f
xcoreashu <connorashutosh@gmail.com>
2018-06-12 14:18:43 +0530

    3rd commit

---
+++ client/src/components/SubmitForm.js
@@ -0,0 +1,12 @@
+import React , { Component } from 'react';
+import __ from 'lodash';
+import { reduxForm , Field } from 'redux-form';
+
+class SubmitForm extends Component {
+  render() {
+    return (
+
+    )
+  }
+}
+export default SubmitForm;
---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID: '343780675499-glre78m6j285j8lrsgea4afj8ve51957.apps.googleusercontent.com',
+  googleClientSecret: 'Wk8xzKn3xqpoaIoxJOoERCwv',
+  mongoURI: 'mongodb://ashutosh:sharapova14@ds261479.mlab.com:61479/mernstack-dev',
+  cookieKey: 'qwertyuioplkjhgfdsa',
+  hostKey: 'http://localhost:3000'
+};
