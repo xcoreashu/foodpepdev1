0d4df7dace2c18ac3dd2aa5c7929adcd806036c6
Ashutosh Pattnayak <connorashutosh@gmail.com>
2018-07-01 02:24:47 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID: '343780675499-glre78m6j285j8lrsgea4afj8ve51957.apps.googleusercontent.com',
-  googleClientSecret: 'Wk8xzKn3xqpoaIoxJOoERCwv',
-  mongoURI: 'mongodb://ashutosh:sharapova14@ds261479.mlab.com:61479/mernstack-dev',
-  cookieKey: 'qwertyuioplkjhgfdsa',
-  hostKey: 'http://localhost:3000',
-  sendGridKey: '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'
-};
